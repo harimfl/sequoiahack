0f024311d97a542fe59756a797d9b00488e85e35
Srinath Ramanujan <gr8srinath@gmail.com>
2014-09-21 00:38:02 +0530

    Adding gcm registration

--- sendpn.js
+++ sendpn.js
@@ -2,7 +2,7 @@
 	//925617659837
 	////AIzaSyDJu-OKmyz-MOHZSKIURSGSg_gFrsRpAeU
 	var gcmsender = new gcm.Sender('AIzaSyDJu-OKmyz-MOHZSKIURSGSg_gFrsRpAeU');
-	var duid = "APA91bGdeLvW6-BMnbDaQkhIW9yb0q6Iycf3VBvX4-Ng9R7Z2l2S7VHS0C30U8lR6D-nbRC2bxUIJLJKqwSpW-gzxMJEuDAw7kVE7T37_1ZxEJKBpWj0ZZ09Bu3N7H0d_rRfW_iCYH6M3GVz_zBevzND-uqetSaOjA";
+    var duid = "APA91bGZi40M_iQUzPIAHRrRsfmyREAsCs_Yfg5GCiIuBxz_iDwJzdal2Y0qe9Dh8u0aUNltGNjlSeA4j6Vfqyc6qivN2YWBm3qRnxbj41GJCAFNx3-cafnMdWNrbc8MaAxJQroZH2Pj1F2aQwYd9IlulM7Es5HdCQ";
 	
 var msg = new gcm.Message();
                     
@@ -24,12 +24,6 @@ var msg = new gcm.Message();
                     msg.addData('param5', "1123");
                     msg.addData('param6', "1283286538");
                     msg.addData('ts',Date.now());
-  
-	        	
-                    
-
-
-
 
 // Statics.sendPushNotif(4, 38263915, "12", "Teen Patti Gold", 
 //                                             "1", text, "12", 1283286538, ['13','26','39'], 1, 0);
@@ -38,7 +32,7 @@ var msg = new gcm.Message();
 	gcmsender.send(msg, duid, 2, function (err, result) {
               if(err) {
 			//TODO: Sample this?
-			console.log("Gitesh sent pn fail");
+			console.log("Gitesh sent pn fail", err);
 			//StatsController.count.apply(StatsController, [pid, 'push_notif_send', 1, 'fail', notifType, 'android', senderId, err]);
 		} else {
 			console.log("Gitesh sent pn");
