8cc9e0eaf9954c51b798dc822ec418ebf003db5d
Srinath Ramanujan <gr8srinath@gmail.com>
2014-09-21 02:29:55 +0530

    updated pn

--- sendpn.js
+++ sendpn.js
@@ -5,13 +5,33 @@ var duid = "APA91bGZi40M_iQUzPIAHRrRsfmyREAsCs_Yfg5GCiIuBxz_iDwJzdal2Y0qe9Dh8u0a
 var msg = new gcm.Message();
 msg.collapseKey = '2';
 msg.delayWhileIdle = true;
-msg.timeToLive = parseInt(1000);   
+msg.timeToLive = parseInt(1000);
 //1 -- You Ran Over <param>
 //2 -- <param> Ran You Over
 //3 -- You Ran Over <param> People
 //4 -- <param> People Ran Over You
-msg.addData('type', 2);	        		
-msg.addData('param', "")
+switch(type) {
+    case 1: {
+        msg.addData('type', 1); 
+        msg.addData('other_name', "Rads");
+        msg.addData('traveller_snuid', "100004176944370");
+        msg.addData('other_snuid', "524740442");
+    } break;
+    case 2: {
+        msg.addData('type', 2); 
+        msg.addData('traveller_name', "Srinath");
+        msg.addData('traveller_snuid', "524740442");
+        msg.addData('other_snuid', "100004176944370");
+    } break;
+    case 3: {
+        msg.addData('type', 3); 
+        msg.addData('num_people', "11");
+    } break;
+    case 4: {
+        msg.addData('type', 4); 
+        msg.addData('num_people', "13");
+    } break;    
+}
 msg.addData('ts',Date.now());
 
 gcmsender.send(msg, [duid], 2, function (err, result) {
