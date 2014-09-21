120ea14b68e2772e23ad7ea9158aa20a2774df2a
Srinath Ramanujan <gr8srinath@gmail.com>
2014-09-21 08:25:39 +0530

    PN on client

--- OlaWars/src/com/seqhack/olawars/GCMIntentService.java
+++ OlaWars/src/com/seqhack/olawars/GCMIntentService.java
@@ -1,11 +1,26 @@
 package com.seqhack.olawars;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -62,38 +77,114 @@ public class GCMIntentService extends GCMBaseIntentService
         PendingIntent pIntent = PendingIntent.getBroadcast(GCMIntentService.this, 1, new Intent(GCMIntentService.this, NotificationView.class), PendingIntent.FLAG_CANCEL_CURRENT);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this).setSmallIcon(R.drawable.icon).setAutoCancel(true).setContentIntent(pIntent).setContent(remoteViews);
         
-        String other_name, traveller_snuid, other_snuid, traveller_name, num_people;
+        String other_name, traveller_snuid, other_snuid, traveller_name, num_people, ssa;
+        HttpGet verifyRequest;
+        DefaultHttpClient client;
         
         switch(type) {
         case 1:
-        	other_name = b.getString("other_name");
         	traveller_snuid = b.getString("traveller_snuid");
-        	other_snuid = b.getString("other_snuid");
-        	remoteViews.setViewVisibility(R.id.cardback1, View.VISIBLE);
-        	remoteViews.setViewVisibility(R.id.cardback2, View.INVISIBLE);
-        	remoteViews.setViewVisibility(R.id.cardback3, View.VISIBLE);
-        	remoteViews.setViewVisibility(R.id.cardback33, View.VISIBLE);
-        	remoteViews.setViewVisibility(R.id.cardback4, View.INVISIBLE);
-        	remoteViews.setViewVisibility(R.id.cardback5, View.VISIBLE);
-        	remoteViews.setViewVisibility(R.id.cardback55, View.VISIBLE);
-        	remoteViews.setTextViewText(R.id.notiftext1, "You Ran Over " + other_name);
-        	remoteViews.setImageViewBitmap(R.id.cardback33, getBitmapFromURL("https://graph.facebook.com/" + traveller_snuid + "/picture?type=normal&height=150&width=150", traveller_snuid));
-        	remoteViews.setImageViewBitmap(R.id.cardback55, getBitmapFromURL("https://graph.facebook.com/" + other_snuid + "/picture?type=normal&height=150&width=150", other_snuid));
+        	other_snuid = b.getString("other_snuid"); //figure out other_name from other_snuid
+        	other_name = "";
+        	ssa = "http://graph.facebook.com/" + other_snuid;
+            verifyRequest = new HttpGet(ssa);  
+            client = new DefaultHttpClient();
+            try {                
+                HttpResponse response = client.execute(verifyRequest);
+                if(response.getStatusLine().getStatusCode() == 200) {
+                    HttpEntity entity = response.getEntity();
+                    if(entity != null){
+                        InputStream inputStream = entity.getContent();
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                        StringBuilder stringBuilder = new StringBuilder();
+
+                        String ligneLue = bufferedReader.readLine();
+                        while(ligneLue != null){
+                            stringBuilder.append(ligneLue + " \n");
+                            ligneLue = bufferedReader.readLine();
+                        }
+                        bufferedReader.close();
+                        final String params = stringBuilder.toString();
+                        
+                    	JSONParser parser=new JSONParser();
+                        Object obj = null;
+                		try {
+                			obj = parser.parse(params);
+                			JSONObject jsonObject = (JSONObject)obj;
+                            other_name = jsonObject.get("first_name").toString();
+                		} catch (ParseException e) {
+                			// TODO Auto-generated catch block
+                			e.printStackTrace();
+                		}
+                    	remoteViews.setViewVisibility(R.id.cardback1, View.VISIBLE);
+                    	remoteViews.setViewVisibility(R.id.cardback2, View.INVISIBLE);
+                    	remoteViews.setViewVisibility(R.id.cardback3, View.VISIBLE);
+                    	remoteViews.setViewVisibility(R.id.cardback33, View.VISIBLE);
+                    	remoteViews.setViewVisibility(R.id.cardback4, View.INVISIBLE);
+                    	remoteViews.setViewVisibility(R.id.cardback5, View.VISIBLE);
+                    	remoteViews.setViewVisibility(R.id.cardback55, View.VISIBLE);
+                    	remoteViews.setTextViewText(R.id.notiftext1, "You Ran Over " + other_name);
+                    	remoteViews.setImageViewBitmap(R.id.cardback33, getBitmapFromURL("https://graph.facebook.com/" + traveller_snuid + "/picture?type=normal&height=150&width=150", traveller_snuid));
+                    	remoteViews.setImageViewBitmap(R.id.cardback55, getBitmapFromURL("https://graph.facebook.com/" + other_snuid + "/picture?type=normal&height=150&width=150", other_snuid));
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         	break;
         case 2:
-        	traveller_name = b.getString("traveller_name");
-        	traveller_snuid = b.getString("traveller_snuid");
+        	traveller_snuid = b.getString("traveller_snuid"); //figure out traveller_name from traveller_snuid
         	other_snuid = b.getString("other_snuid");
-        	remoteViews.setViewVisibility(R.id.cardback1, View.VISIBLE);
-        	remoteViews.setViewVisibility(R.id.cardback2, View.INVISIBLE);
-        	remoteViews.setViewVisibility(R.id.cardback3, View.VISIBLE);
-        	remoteViews.setViewVisibility(R.id.cardback33, View.VISIBLE);
-        	remoteViews.setViewVisibility(R.id.cardback4, View.INVISIBLE);
-        	remoteViews.setViewVisibility(R.id.cardback5, View.VISIBLE);
-        	remoteViews.setViewVisibility(R.id.cardback55, View.VISIBLE);
-        	remoteViews.setTextViewText(R.id.notiftext1, traveller_name + " Ran you over");
-        	remoteViews.setImageViewBitmap(R.id.cardback33, getBitmapFromURL("https://graph.facebook.com/" + traveller_snuid + "/picture?type=normal&height=150&width=150", traveller_snuid));
-        	remoteViews.setImageViewBitmap(R.id.cardback55, getBitmapFromURL("https://graph.facebook.com/" + other_snuid + "/picture?type=normal&height=150&width=150", other_snuid));
+        	traveller_name = "";
+        	
+        	ssa = "http://graph.facebook.com/" + traveller_snuid;
+            verifyRequest = new HttpGet(ssa);  
+            client = new DefaultHttpClient();
+            try {                
+                HttpResponse response = client.execute(verifyRequest);
+                if(response.getStatusLine().getStatusCode() == 200) {
+                    HttpEntity entity = response.getEntity();
+                    if(entity != null){
+                        InputStream inputStream = entity.getContent();
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                        StringBuilder stringBuilder = new StringBuilder();
+
+                        String ligneLue = bufferedReader.readLine();
+                        while(ligneLue != null){
+                            stringBuilder.append(ligneLue + " \n");
+                            ligneLue = bufferedReader.readLine();
+                        }
+                        bufferedReader.close();
+                        final String params = stringBuilder.toString();
+                        
+                    	JSONParser parser=new JSONParser();
+                        Object obj = null;
+                		try {
+                			obj = parser.parse(params);
+                			JSONObject jsonObject = (JSONObject)obj;
+                            other_name = jsonObject.get("first_name").toString();
+                		} catch (ParseException e) {
+                			// TODO Auto-generated catch block
+                			e.printStackTrace();
+                		}
+                		
+                    	remoteViews.setViewVisibility(R.id.cardback1, View.VISIBLE);
+                    	remoteViews.setViewVisibility(R.id.cardback2, View.INVISIBLE);
+                    	remoteViews.setViewVisibility(R.id.cardback3, View.VISIBLE);
+                    	remoteViews.setViewVisibility(R.id.cardback33, View.VISIBLE);
+                    	remoteViews.setViewVisibility(R.id.cardback4, View.INVISIBLE);
+                    	remoteViews.setViewVisibility(R.id.cardback5, View.VISIBLE);
+                    	remoteViews.setViewVisibility(R.id.cardback55, View.VISIBLE);
+                    	remoteViews.setTextViewText(R.id.notiftext1, traveller_name + " Ran you over");
+                    	remoteViews.setImageViewBitmap(R.id.cardback33, getBitmapFromURL("https://graph.facebook.com/" + traveller_snuid + "/picture?type=normal&height=150&width=150", traveller_snuid));
+                    	remoteViews.setImageViewBitmap(R.id.cardback55, getBitmapFromURL("https://graph.facebook.com/" + other_snuid + "/picture?type=normal&height=150&width=150", other_snuid));
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        	
         	break;
         case 3:
         	num_people = b.getString("num_people");
@@ -106,6 +197,7 @@ public class GCMIntentService extends GCMBaseIntentService
         	remoteViews.setViewVisibility(R.id.cardback55, View.INVISIBLE);
         	remoteViews.setTextViewText(R.id.notiftext1, "You Ran Over " + num_people + " People");
         	break;
+        	
         case 4:
         	num_people = b.getString("num_people");
         	remoteViews.setViewVisibility(R.id.cardback1, View.VISIBLE);
